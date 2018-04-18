a1c51826dd799bcd417dd5ba6db3e2cdb2a59a78
Christoph Theis <christoph_theis@outlook.com>
2018-04-18 22:20:45 +0100

    First steps of FT for layout + styling

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -12,6 +12,7 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser = webdriver.Firefox()
     
     def tearDown(self):
+        self.browser.refresh()
         self.browser.quit()
     
     def wait_for_row_in_list_table(self, row_text):
@@ -79,6 +80,30 @@ class NewVisitorTest(LiveServerTestCase):
         self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
         
         # Satisfied, she goes back to sleep
+
+    def test_layout_and_styling(self):
+        # Edith goes to the homepage
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # She notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] /2, 512, 
+            delta=10
+        )
+
+        # She starts a new list and sees the input is nicely centered there too
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] /2, 512, 
+            delta=10
+        )
+
         
     def test_multiple_users_can_start_lists_at_different_urls(self):
         # Edith starts a new to-do list

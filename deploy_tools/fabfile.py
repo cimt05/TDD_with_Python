cf3a7202ded3b3ed89afacb23acd0716153f2f75
Christoph Theis <christoph_theis@outlook.com>
2018-05-11 10:16:27 +0100

    Move unit tests into a folder with single file

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -2,7 +2,7 @@ from fabric.contrib.files import append, exists, sed
 from fabric.api import env, local, run
 import random
 
-REPO_URL = 'https.//github.com/cimt05/TDD_with_Python.git'
+REPO_URL = 'https://github.com/cimt05/TDD_with_Python.git'
 
 def deploy():
     site_folder = f'/home/{env.user}/sites/{env.host}'
@@ -13,7 +13,6 @@ def deploy():
     _update_virtualenv(source_folder)
     _update_static_files(source_folder)
     _update_database(source_folder)
-
 
 def _create_directory_structure_if_necessary(site_folder):
     for subfolder in ('database', 'static', 'virtualenv', 'source'):

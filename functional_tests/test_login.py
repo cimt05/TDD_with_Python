246bdf73a03e9ce779da2815c8afbd8e24f1eedb
Christoph Theis <christoph_theis@outlook.com>
2018-06-02 15:26:33 +0100

    really skip login test on staging server

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -24,38 +24,38 @@ class LoginTest(FunctionalTest):
         if not self.staging_server:
             test_email = 'edith@example.com'
 
-        self.browser.get(self.live_server_url)
-        self.browser.find_element_by_name('email').send_keys(test_email)
-        self.browser.find_element_by_name('email').send_keys(Keys.ENTER)
+            self.browser.get(self.live_server_url)
+            self.browser.find_element_by_name('email').send_keys(test_email)
+            self.browser.find_element_by_name('email').send_keys(Keys.ENTER)
+            
+            # A message appears telling her an email has been sent
+            self.wait_for(lambda: self.assertIn(
+                'Check your email',
+                self.browser.find_element_by_tag_name('body').text
+            ))
 
-        # A message appears telling her an email has been sent
-        self.wait_for(lambda: self.assertIn(
-            'Check your email',
-            self.browser.find_element_by_tag_name('body').text
-        ))
+            # She checks her email and finds a message
+            body = self.wait_for_email(test_email, SUBJECT)
 
-        # She checks her email and finds a message
-        body = self.wait_for_email(test_email, SUBJECT)
-
-        # It has a url link in it
-        self.assertIn('Use this link to log in', body)
-        url_search = re.search(r'http://.+/.+$', body)
-        if not url_search:
-            self.fail(f'Could not find url in email body:\n{body}')
-        url = url_search.group(0)
-        self.assertIn(self.live_server_url, url)
-
-        # She clicks it
-        self.browser.get(url)
-
-        # She is logged in!
-        self.wait_to_be_logged_in(email=test_email)
-
-        # Now she logs out
-        self.browser.find_element_by_link_text('Log out').click()
-
-        # She is logged out
-        self.wait_to_be_logged_out(email=test_email)
+            # It has a url link in it
+            self.assertIn('Use this link to log in', body)
+            url_search = re.search(r'http://.+/.+$', body)
+            if not url_search:
+                self.fail(f'Could not find url in email body:\n{body}')
+            url = url_search.group(0)
+            self.assertIn(self.live_server_url, url)
+            
+            # She clicks it
+            self.browser.get(url)
+            
+            # She is logged in!
+            self.wait_to_be_logged_in(email=test_email)
+            
+            # Now she logs out
+            self.browser.find_element_by_link_text('Log out').click()
+            
+            # She is logged out
+            self.wait_to_be_logged_out(email=test_email)
 
 
     def wait_for_email(self, test_email, subject):
